d519a7b7cb1cf6b3fdfbbfbf0443ad0ca71b7d82
ChanPark <thechanmoon@gmail.com>
2020-04-23 20:03:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,51 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component
+{
+    state =
+    {
+        astronauts: []
+    }
+
+    componentDidMount()
+    {
+       fetch('http://api.open-notify.org/astros.json')
+       .then(res=>res.json())
+       .then(data => {
+           console.log('data : ', data.people);
+
+           this.setState({astronauts: data.people});
+       })     
+    }
+
+    getAstraunautsName()
+    {
+        // debugger
+        console.log('getAstraunautsName', this.state.astronauts);
+        let retVal = this.state.astronauts.map( (astronaut) => <li>{astronaut.name}</li>)
+        // debugger
+        return retVal;
+    }
+
+    render()
+    {
+    return(
+        <div>
+
+            {/* Hello World */}
+            {this.getAstraunautsName()}
+            
+        </div>
+        )
+    }
+}
+
+export default App;
+
+
+// return (
+//     <div>
+//         Hello World
+//     </div>
+//   );
